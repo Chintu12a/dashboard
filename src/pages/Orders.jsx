8b9661f71bd238cdfa01c6b52777b6ecbc123092
Chintu12a <chintulekurwale@gmail.com>
2023-09-16 17:20:05 +0530

    export fiel changes

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -3,26 +3,43 @@ import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, Context
 
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
-import { pdfExport } from '@syncfusion/ej2/pivotview';
+import { excelExport, pdfExport } from '@syncfusion/ej2/pivotview';
 
 const Orders = () => {
+  let grid;
+  const toolbar = ['PdfExport', "ExcelExport"];
+  const toolbarClick = (args) => {
+    if (grid) {
+      if (args.item.id === 'grid_pdfexport') {
+        grid.pdfExport();
+      }
+      else if (args.item.id == 'grid_excelexport') {
+        grid.pdfExport();
+      }
+    }
+  };
+
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Orders"/>
+      <Header category="Page" title="Orders" />
       <GridComponent
-      id='gridcomp'
-      allowPaging
+        id='grid'
+        allowPaging
         allowSorting
-        allowExcelExport
-        allowPdfExport
+        allowExcelExport={true}
+        allowPdfExport={true}
         contextMenuItems={contextMenuItems}
+        dataSource={ordersData}
+        toolbar={toolbar}
+        toolbarClick={toolbarClick}
+        ref={g => grid = g}
       >
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
-        <Inject services={[ContextMenu, Edit, Sort, Filter,Page, ExcelExport, PdfExport]} />
+        <Inject services={[ContextMenu, Edit, Sort, Filter, Page, ExcelExport, PdfExport]} />
       </GridComponent>
     </div>
   )
